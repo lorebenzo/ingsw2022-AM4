b35861fe9ece606f230a5c5c7f7ca535a2a21958
Ax-Time <58958580+Ax-Time@users.noreply.github.com>
2022-03-27 18:13:32 +0200

    solved issue #15

--- src/main/java/it/polimi/ingsw/server/game_logic/GameState.java
+++ src/main/java/it/polimi/ingsw/server/game_logic/GameState.java
@@ -199,8 +199,6 @@ public class GameState {
         currentPlayerSchoolBoard.grabStudentsFromCloud(studentsGrabbed);
     }
 
-
-
     /**
      * The current player plays the given card
      * @requires school board ID exists
