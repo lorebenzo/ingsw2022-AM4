16d4c5645ee7cdd156941dcdb83d1402ca722aed
lorebenzo <49951981+lorebenzo@users.noreply.github.com>
2022-04-15 17:40:47 +0200

    Delete src/main/java/it/polimi/ingsw/server/communication/sugar directory

--- src/main/java/it/polimi/ingsw/server/communication/sugar/MessageType.java
+++
@@ -1,5 +0,0 @@
-package it.polimi.ingsw.server.communication.sugar;
-
-public enum MessageType {
-    JOIN, CONTROL, ACTION, NOTIFY, HEARTBEAT
-}
--- src/main/java/it/polimi/ingsw/server/communication/sugar/exceptions/MessageSerializationException.java
+++
@@ -1,6 +0,0 @@
-package it.polimi.ingsw.server.communication.sugar.exceptions;
-
-public class MessageSerializationException extends Throwable {
-    public MessageSerializationException(String message) {
-    }
-}
