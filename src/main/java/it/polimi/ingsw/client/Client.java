9973bb0d6e8afc5711f87999a8d6882cc2329687
lorenzobenzoni <lorenzo1.benzoni@mail.polimi.it>
2022-05-24 00:26:26 +0200

    fix(client)

--- src/main/java/it/polimi/ingsw/client/Client.java
+++ src/main/java/it/polimi/ingsw/client/Client.java
@@ -10,15 +10,32 @@ public class Client {
     public static void main(String[] args) throws IOException, InterruptedException {
         var gameClient = new GameClient();
 
+
         Thread t = new Thread(gameClient);
 
         t.start();
 
-        gameClient.parseLine("join-matchmaking --players=2 --expert=false");
+
+//        gameClient.login("mauro", "bro");
+//        gameClient.signUp("player", "bro");
+
+
+//        gameClient.parseLine("join-matchmaking --players=2 --expert=false");
 
         while(true) {
             var br = new BufferedReader(new InputStreamReader(System.in));
             gameClient.parseLine(br.readLine());
         }
     }
+
+    public static class Server {
+        public static void main(String[] args) throws IOException {
+            var gameServer = new GameServer();
+
+            Thread threadServer = new Thread(gameServer);
+            threadServer.start();
+        }
+    }
 }
+
+
