8fb0dedb2b3c911b7deaeb14761d205e723944b3
lorenzobenzoni <lorenzo1.benzoni@mail.polimi.it>
2022-05-25 00:29:08 +0200

    feat(jar): server jar

--- src/main/java/it/polimi/ingsw/server/App.java
+++ src/main/java/it/polimi/ingsw/server/App.java
@@ -1,7 +1,15 @@
 package it.polimi.ingsw.server;
 
-public class App {
-    public static void main(String[] args) {
+import com.google.gson.Gson;
+import it.polimi.ingsw.server.server_logic.GameServer;
 
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        GameServer gs = new GameServer();
+        Thread t = new Thread(gs);
+
+        t.start();
     }
 }
