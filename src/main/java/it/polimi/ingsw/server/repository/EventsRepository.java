3d6494160e1dffc78540420d905eb116fccd20d1
lorenzobenzoni <lorenzo1.benzoni@mail.polimi.it>
2022-06-27 20:08:07 +0200

    fix tests

--- src/main/java/it/polimi/ingsw/server/repository/EventsRepository.java
+++ src/main/java/it/polimi/ingsw/server/repository/EventsRepository.java
@@ -41,128 +41,142 @@ public class EventsRepository {
     }
 
     public void rollback(UUID aggregateUUID, int aggregateVersion) throws DBQueryException {
-        try {
-            Statement stmt = c.createStatement();
+        if(instance != null)
+            try {
+                Statement stmt = c.createStatement();
 
-            String sql = "delete\n" +
-                        "from event.events\n" +
-                        "where aggregate_id = '" + aggregateUUID + "'\n" +
-                        "and aggregate_version >= " + aggregateVersion;
+                String sql = "delete\n" +
+                            "from event.events\n" +
+                            "where aggregate_id = '" + aggregateUUID + "'\n" +
+                            "and aggregate_version >= " + aggregateVersion;
 
-            stmt.executeUpdate(sql);
-            stmt.close();
-        } catch (SQLException e) {
-            throw new DBQueryException();
-        }
+                stmt.executeUpdate(sql);
+                stmt.close();
+            } catch (SQLException e) {
+                throw new DBQueryException();
+            }
     }
 
     public void storeAggregate(UUID uuid, String aggregateClass) throws SQLException {
-        Statement stmt = c.createStatement();
-        // todo: rendere genrico
-        String sql = "INSERT INTO event.aggregates (id, type)\n" + "VALUES ('" + uuid + "', '"+ 1 +"');";
-        stmt.executeUpdate(sql);
-        stmt.close();
-    }
-
-    public void generateSnapshot(Aggregate aggregate) throws DBQueryException {
-        try {
+        if(instance != null) {
             Statement stmt = c.createStatement();
-
-            String sql = "INSERT INTO event.snapshots (aggregate_id, version, snap)\n"
-                    + "VALUES ('"
-                    + aggregate.id + "', '"
-                    + aggregate.version + "', '"
-                    + SerDes.serialize(aggregate) + "');";
-
+            String sql = "INSERT INTO event.aggregates (id, type)\n" + "VALUES ('" + uuid + "', '"+ 1 +"');";
             stmt.executeUpdate(sql);
             stmt.close();
-        } catch (SQLException e) {
-            throw new DBQueryException();
         }
     }
 
+    public void generateSnapshot(Aggregate aggregate) throws DBQueryException {
+        if(instance != null)
+            try {
+                Statement stmt = c.createStatement();
+
+                String sql = "INSERT INTO event.snapshots (aggregate_id, version, snap)\n"
+                        + "VALUES ('"
+                        + aggregate.id + "', '"
+                        + aggregate.version + "', '"
+                        + SerDes.serialize(aggregate) + "');";
+
+                stmt.executeUpdate(sql);
+                stmt.close();
+            } catch (SQLException e) {
+                throw new DBQueryException();
+            }
+    }
+
     public String getAggregateType(UUID uuid) throws DBQueryException {
-        String type;
-        try {
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery( "SELECT * FROM event.aggregates INNER JOIN event.aggregate_type ON type=aggregate_id "
-                                                + "WHERE id = '" + uuid + "'");
-
-            rs.next();
-            type = rs.getString("aggregate_name");
-
-            rs.close();
-            stmt.close();
-        } catch(SQLException ignored) {
-            ignored.printStackTrace();
-            throw new DBQueryException();
+        String type = null;
+        if(instance != null) {
+            try {
+                Statement stmt = c.createStatement();
+                ResultSet rs = stmt.executeQuery( "SELECT * FROM event.aggregates INNER JOIN event.aggregate_type ON type=aggregate_id "
+                                                    + "WHERE id = '" + uuid + "'");
+    
+                rs.next();
+                type = rs.getString("aggregate_name");
+    
+                rs.close();
+                stmt.close();
+            } catch(SQLException ignored) {
+                ignored.printStackTrace();
+                throw new DBQueryException();
+            }
         }
         return type;
     }
 
     public void addEvent(UUID aggregateUuid, Event event, int aggregateVersion) throws DBQueryException {
-        try {
-            Statement stmt = c.createStatement();
-            String sql = "INSERT INTO event.events (ID, event, aggregate_id, event_class, event_parent_uuid, aggregate_version)\n"
+        if(instance != null) {
+            try {
+                Statement stmt = c.createStatement();
+                String sql = "INSERT INTO event.events (ID, event, aggregate_id, event_class, event_parent_uuid, aggregate_version)\n"
                         + "VALUES ('"
                         + event.id + "', '"
-                        + SerDes.serialize(event) +"', '"
+                        + SerDes.serialize(event) + "', '"
                         + aggregateUuid + "', '"
-                        + EventsMapper.getKeyByValue(event.getClass()) +"', '"
+                        + EventsMapper.getKeyByValue(event.getClass()) + "', '"
                         + event.parentEvent + "', '"
                         + aggregateVersion + "');";
 
 
-            stmt.executeUpdate(sql);
-            stmt.close();
-        } catch(SQLException ignored) {
-            throw new DBQueryException();
+                stmt.executeUpdate(sql);
+                stmt.close();
+            } catch (SQLException ignored) {
+                throw new DBQueryException();
+            }
         }
     }
 
     public Collection<Event> getEvents(UUID uuid, int aggregateVersion) throws DBQueryException {
+        if(instance != null) {
+            Collection<Event> events = new Stack<>();
 
-        Collection<Event> events = new Stack<>();
+            try {
+                Statement stmt = c.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM event.events "
+                        + "WHERE aggregate_id = '"
+                        + uuid + "' " +
+                        "and aggregate_version > " + aggregateVersion + ";");
 
-        try {
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM event.events "
-                    + "WHERE aggregate_id = '"
-                    + uuid + "' " +
-                    "and aggregate_version > " + aggregateVersion + ";");
-
-            while (rs.next()) {
-                String event = rs.getString("event");
-                Integer eventClass = rs.getInt("event_class");
-                Event eventParsed = (Event) SerDes.deserialize(event, EventsMapper.getFromId(eventClass));
-                events.add(eventParsed);
+                while (rs.next()) {
+                    String event = rs.getString("event");
+                    Integer eventClass = rs.getInt("event_class");
+                    Event eventParsed = (Event) SerDes.deserialize(event, EventsMapper.getFromId(eventClass));
+                    events.add(eventParsed);
+                }
+                rs.close();
+                stmt.close();
+            } catch (SQLException ignored) {
+                throw new DBQueryException();
             }
-            rs.close();
-            stmt.close();
-        } catch(SQLException ignored) {
-            throw new DBQueryException();
+            return events;
+        } else {
+            return new Stack<>();
         }
-        return events;
     }
 
     public Aggregate getSnapshot(UUID aggregateID) throws SQLException {
-        Statement stmt = c.createStatement();
-        ResultSet rs = stmt.executeQuery("select * " +
-                "from event.snapshots " +
-                "where aggregate_id = '" + aggregateID + "' " +
-                "order by version desc " +
-                "fetch first 1 rows only;" );
+        if(instance != null) {
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("select * " +
+                    "from event.snapshots " +
+                    "where aggregate_id = '" + aggregateID + "' " +
+                    "order by version desc " +
+                    "fetch first 1 rows only;");
 
-        Aggregate aggregateParsed = null;
-        int version = 0;
-        while ( rs.next() ) {
-            String aggregate = rs.getString("snap");
-            version = rs.getInt("version");
+            Aggregate aggregateParsed = null;
+            int version = 0;
+            while (rs.next()) {
+                String aggregate = rs.getString("snap");
+                version = rs.getInt("version");
 
-            aggregateParsed = (GameState) SerDes.deserialize(aggregate, GameState.class);
+                aggregateParsed = (GameState) SerDes.deserialize(aggregate, GameState.class);
+            }
+            rs.close();
+            stmt.close();
+            return aggregateParsed;
+        } else {
+            return null;
         }
-        rs.close();
-        stmt.close();
-        return aggregateParsed;
     }
 }
